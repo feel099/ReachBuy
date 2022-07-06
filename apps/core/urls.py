a051f7974c6ca1cc39309b7b9afcbe0e4cfece31
Alexandr Filippov <feel099@yandex.ru>
2022-07-06 17:09:57 +0300

    Base functional

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import base, about
 
 urlpatterns = [
-    path('', index, name='reachbuy'),
+    path('', base, name='reachbuy'),
+    path('about/', about, name='about'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html')
+def base(request):
+    return render(request, 'base.html')
+
+def about(request):
+    return render(request, 'about.html')
